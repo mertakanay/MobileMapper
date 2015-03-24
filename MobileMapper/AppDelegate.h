ad33a3b0754b78041bc5c07995c0a54d93b1ac01
mertakanay <mert.akanay@live.com>
2015-03-24 10:40:27 -0700

    Initial Commit

---
+++ MobileMapper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobileMapper
+//
+//  Created by Mert Akanay on 24.03.2015.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobileMapper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobileMapper
+//
+//  Created by Mert Akanay on 24.03.2015.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
